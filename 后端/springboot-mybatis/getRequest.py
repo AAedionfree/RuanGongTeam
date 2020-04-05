c7b5a220a95d2a3eaa1328e75f1a41cb75aba4bf
AAedion <AAedion@buaa.edu.cn>
2020-04-05 13:29:57 +0800

    change python code

--- 后端/springboot-mybatis/getRequest.py
+++ 后端/springboot-mybatis/getRequest.py
@@ -1,8 +1,10 @@
 import requests
 import json
-url = "http://localhost:8080"
-url = url + "/api/city" + "?cityName=温州市"
-response = requests.get(url).text
-text = json.loads(response)
-# <class 'list'> <class 'dict'>
-print(type(text),type(text[0]))
+if __name__ == '__main__':
+    url = "http://localhost:8081"
+    url = url + "/api/city" + "?cityName=温岭市"
+    response = requests.get(url).text
+    text = json.loads(response)
+    # <class 'list'> <class 'dict'>
+    print(type(text),type(text[0]))
+    print(text)
