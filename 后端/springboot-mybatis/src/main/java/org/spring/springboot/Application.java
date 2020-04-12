bf8ac4e711c7eb68e0c9012e749500a41c5c6edb
AAedion <AAedion@buaa.edu.cn>
2020-04-12 20:25:08 +0800

    commit

--- 后端/springboot-mybatis/src/main/java/org/spring/springboot/Application.java
+++ 后端/springboot-mybatis/src/main/java/org/spring/springboot/Application.java
@@ -13,11 +13,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 // mapper 接口类扫描包配置
 @MapperScan("org.spring.springboot.dao")
-public class Application {
+    public class Application {
 
-    public static void main(String[] args) {
-        // 程序启动入口
-        // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
-        SpringApplication.run(Application.class,args);
-    }
+        public static void main(String[] args) {
+            // 程序启动入口
+            // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
+            SpringApplication.run(Application.class,args);
+        }
 }
