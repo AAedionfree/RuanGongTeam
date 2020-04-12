a0252c2885e723527f18112b0328c8390ec2df71
sunhaoran <164878954@qq.com>
2020-04-12 19:15:56 +0800

    select * from user增添通过userId查找方式

--- 后端/springboot-mybatis/src/main/java/org/spring/springboot/controller/UserController.java
+++ 后端/springboot-mybatis/src/main/java/org/spring/springboot/controller/UserController.java
@@ -15,8 +15,12 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "/api/user", method = RequestMethod.GET)
+    @RequestMapping(value = "/api/userAccount", method = RequestMethod.GET)
     List<User> findUserByUserAccount(@RequestParam(value = "userAccount", required = true) String userAccount){
         return userService.findUserByUserAccount(userAccount);
+    }
+    @RequestMapping(value = "/api/userId", method = RequestMethod.GET)
+    List<User> findUserByUserId(@RequestParam(value = "userId", required = true) String userId){
+        return userService.findUserByUserId(userId);
     }
 }
--- 后端/springboot-mybatis/src/main/java/org/spring/springboot/service/UserService.java
+++ 后端/springboot-mybatis/src/main/java/org/spring/springboot/service/UserService.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface UserService {
     List<User> findUserByUserAccount(String userId);
+    List<User> findUserByUserId(String userId);
 }
