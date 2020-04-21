a10ba7ab57b6789e4d7c620041d608511aff9fc7
wangrunxin <1375090938@git>
2020-04-21 16:41:54 +0800

    完成登录功能

---
+++ mid/shebeimanage2/app/src/main/java/com/example/shebeimanage2/activity/searchinfo.java
@@ -0,0 +1,7 @@
+package com.example.shebeimanage2.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+class searchinfo extends AppCompatActivity {
+
+}
--- mid/shebeimanage2/app/src/main/java/com/example/shebeimanage2/activity/splsh.java
+++ mid/shebeimanage2/app/src/main/java/com/example/shebeimanage2/activity/splsh.java
@@ -1,4 +1,30 @@
 package com.example.shebeimanage2.activity;
 
-public class splsh {
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.shebeimanage2.R;
+
+
+public class splsh extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.splsh);
+
+//        Button search = findViewById(R.id.);
+//        search.setOnClickListener(
+//                v ->
+//                {
+//                    new Thread(()-> {
+//                        startActivity(new Intent(splsh.this,searchinfo.class ));
+//                    }
+//                }
+//
+//        );
+
+    }
 }
