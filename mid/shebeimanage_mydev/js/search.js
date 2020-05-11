b21397efb842ab0c8725fef0e54135908316d720
Lazy-i <55023805+Lazy-i@users.noreply.github.com>
2020-05-11 23:07:03 +0800

    更新了与租借相关的一系列中间代码

--- mid/shebeimanage_mydev/js/search.js
+++ mid/shebeimanage_mydev/js/search.js
@@ -34,3 +34,43 @@ function search_m(request_url){
 				 });
 	        });
 }
+
+function search_s(dev_id,request_url){
+	mui.init();
+	mui.plusReady(function(){
+	            var self = plus.webview.currentWebview();
+				 var user = self.user;
+				 
+				 // var s ="";
+				 	
+				 // for (var p in user) {
+				 // 	s= s+"n "+p+": "+user[p];
+				 // }
+				 // alert(s);
+				 var search_url=request_url+"devLend?userAccount="+user.userAccount+"&devId="+dev_id;
+				 // alert(search_url);
+				 mui.ajax({
+					 type:'GET',
+					 url:search_url,
+					 timeout:10000,	
+					 dataType:"json",
+					 success:function(data){
+							
+						if(data.data!=null){
+							var dev_data = new Array();
+							dev_data=data.data;
+							alert("租借成功");
+							search_m(request_url);
+						}
+						else{
+							alert(dev_data);
+							print(dev_data);
+							search_m(request_url);
+						}
+					 },
+					 error: function(xhr,type,errorThrown){
+					 	alert("服务器内部出错！");
+					 }
+				 });
+	        });
+}
