ae7992ccdc18ca5641c7ea4bfda6c095bd9e0cd6
wangrunxin <1375090938@git>
2020-05-29 17:00:29 +0800

    我的设备更多

--- mid/shebeimanage_mydev/js/myequ.js
+++ mid/shebeimanage_mydev/js/myequ.js
@@ -100,7 +100,52 @@ function more(dev_data_num,request_url){
 	// }
 	// alert(s);
 }
-
+function more_zujie(dev_data_num,request_url){
+	mui.init();
+	mui.plusReady(function(){
+	            var self = plus.webview.currentWebview();
+				 var user = self.user;
+				 
+				 // var s ="";
+				 	
+				 // for (var p in user) {
+				 // 	s= s+"n "+p+": "+user[p];
+				 // }
+				 // alert(s);
+				 var search_zujie_url = request_url+'devDevUserAccount?userAccount='+user.userAccount;
+				 // alert(search_url);
+				 mui.ajax({
+					 type:'GET',
+					 url:search_zujie_url,
+					 timeout:10000,	
+					 dataType:"json",
+					 success:function(data){
+							
+						if((data.data!=null)){
+							var dev_data = new Array();
+							dev_data=data.data;
+							var dev_chadata=dev_data[dev_data_num];
+							var s ="";
+								
+							for (var p in dev_chadata) {
+								s= s+"\n"+p+": "+dev_chadata[p];
+							}
+							alert(s);
+						}
+					 },
+					 error: function(xhr,type,errorThrown){
+					 	mui.toast("服务器内部出错！");
+					 }
+				 });
+	        });
+	
+	// var s ="";
+		
+	// for (var p in dev_data) {
+	// 	s= s+"n "+p+": "+dev_data[p];
+	// }
+	// alert(s);
+}
 	
 function add_xiu(request_url,dev_data_num){
 		mui.init();
