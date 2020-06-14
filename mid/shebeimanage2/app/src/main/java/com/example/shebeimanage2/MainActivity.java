7b8901375a21320859c03fe66c49bf9535fae32a
AAedion <AAedion@buaa.edu.cn>
2020-06-14 20:59:19 +0800

    delete unused file

--- mid/shebeimanage2/app/src/main/java/com/example/shebeimanage2/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package com.example.shebeimanage2;
-
-import androidx.appcompat.app.AppCompatActivity;
-public abstract class MainActivity extends AppCompatActivity {
-
-
-}
--- mid/shebeimanage2/app/src/main/java/com/example/shebeimanage2/activity/searchinfo.java
+++
@@ -1,7 +0,0 @@
-package com.example.shebeimanage2.activity;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-class searchinfo extends AppCompatActivity {
-
-}
