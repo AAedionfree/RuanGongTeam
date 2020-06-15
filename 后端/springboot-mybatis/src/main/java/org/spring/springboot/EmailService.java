d872408311881e925f72e7a955d992d1546a30eb
AAedion <AAedion@buaa.edu.cn>
2020-06-15 16:50:39 +0800

    EmailService

---
+++ 后端/springboot-mybatis/src/main/java/org/spring/springboot/EmailService.java
@@ -0,0 +1,41 @@
+package org.spring.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+/**
+ * The Email service.
+ *
+ * @author felord.cn
+ * @since 2020 /1/14 23:22
+ */
+@Component
+public class EmailService {
+    @Resource
+    private JavaMailSender javaMailSender;
+    @Value("${spring.mail.username}")
+    private String from;
+
+
+    /**
+     * 发送纯文本邮件.
+     *
+     * @param to      目标email 地址
+     * @param subject 邮件主题
+     * @param text    纯文本内容
+     */
+    public void sendMail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        javaMailSender.send(message);
+    }
+}
